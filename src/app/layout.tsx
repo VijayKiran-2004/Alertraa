adc09c48dd48e56afdc317437b4cc26368518020
vijay kiran <vijaykiran1008@gmail.com>
2025-09-03 14:17:08 +0000

    you can use this svg for the app icon as well

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" sizes="any" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
