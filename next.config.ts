89ec3554c5725d9f967f9a6cba42b981c1deb494
vijay kiran <vijaykiran1008@gmail.com>
2025-09-04 17:49:59 +0000

    I see this before when I opened

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,37 @@ const isDev = process.env.NODE_ENV === 'development';
 const withPWA = withPWAInit({
   dest: 'public',
   disable: isDev,
+  runtimeCaching: [
+    {
+      urlPattern: /\.(?:png|jpg|jpeg|svg|ico)$/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'images',
+        expiration: {
+          maxEntries: 60,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+        },
+      },
+    },
+    {
+      urlPattern: /^https?.*/,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'https-calls',
+        networkTimeoutSeconds: 15,
+        expiration: {
+          maxEntries: 150,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+        },
+        cacheableResponse: {
+          statuses: [0, 200],
+        },
+      },
+    },
+  ],
+  fallbacks: {
+    document: '/offline.html',
+  },
 });
 
 const nextConfig: NextConfig = {
