1bf63e4f5705140e4a9c827577005470f7b02cd5
vijay kiran <vijaykiran1008@gmail.com>
2025-09-06 09:06:25 +0000

    lattle more

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -32,7 +32,7 @@ export default function Header({ onMenuClick, isDarkMode, healthStatus, cartCoun
   };
 
   return (
-    <header className={cn('px-4 py-2 flex items-center justify-between sticky top-0 z-20', themeClasses)}>
+    <header className={cn('px-4 py-1 flex items-center justify-between sticky top-0 z-20', themeClasses)}>
       <div className="flex items-center space-x-3">
          <div className={cn('w-4 h-4 rounded-full transition-colors duration-500', getStatusColorClass(healthStatus))} />
          <AlertraaLogo className="!w-20" />
